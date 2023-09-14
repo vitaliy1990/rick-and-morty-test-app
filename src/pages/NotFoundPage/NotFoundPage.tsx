00a19c021eeb097ac89c479754267c60ad00a666
vitaliy1990 <chumakov_vitaliy@meta.ua>
2023-09-14 22:40:47 +0300

    add styles in 404 page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,5 @@
 import React, { FC, memo } from 'react';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import css from './NotFoundPage.module.css';
 
 const NotFoundPage: FC = () => {
@@ -9,7 +9,11 @@ const NotFoundPage: FC = () => {
   return (
     <div className={css.root}>
       <h1>404 Page not found</h1>
-      {stateMessage && <p>{stateMessage}</p>}
+      {stateMessage && (
+        <p className={css.infoMessage}>
+          {stateMessage} <Link to='/'> &#8810; Go Back</Link>
+        </p>
+      )}
     </div>
   );
 };
